c4299ea477259acb33aeaa83b54b7a10c8e4786d
Francisco <fran89rbb@hotmail.com>
2021-06-13 14:38:59 +0200

    Implementando borrar cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -16,6 +17,29 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe(clientes =>
       this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void{
+    Swal.fire({
+      title: '¿Está seguro?',
+      text: `¿Seguro que desea eliminar al cliente ${cliente.nombre}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clienteService.delete(cliente.id).subscribe(resp => {
+          this.clientes = this.clientes.filter(cli => cli !== cliente);
+          Swal.fire(
+            'Cliente eliminado!',
+            `Cliente ${cliente.nombre} eliminado con éxito`,
+            'success'
+          )
+        })    
+      }
+    })
   }
 
 }
